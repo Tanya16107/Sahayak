6f72e682771c9aec2870328ee3c2adc99b0ff56d
Meghna Gupta <meghnagupta64@gmail.com>
2019-09-25 23:16:27 +0530

    Launch and Onboarding Screens added

---
+++ app/src/main/java/com/mobilecomputing/sahayak/Activities/OnboardingActivity.java
@@ -0,0 +1,27 @@
+package com.mobilecomputing.sahayak.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.mobilecomputing.sahayak.R;
+
+public class OnboardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_onboarding);
+
+        Button mcontinueButton = (Button) findViewById(R.id.continueButton);
+
+        mcontinueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/mobilecomputing/sahayak/Fragments/FragmentOnboarding3.java
@@ -0,0 +1,45 @@
+package com.mobilecomputing.sahayak.Fragments;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.fragment.app.Fragment;
+
+import com.mobilecomputing.sahayak.R;
+
+public class FragmentOnboarding3 extends Fragment {
+
+
+    public FragmentOnboarding3() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_onboarding3, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+        ImageView photo = view.findViewById(R.id.image_onboarding3);
+
+        int width = (Resources.getSystem().getDisplayMetrics().widthPixels) / 3;
+        int height = width;
+
+        photo.getLayoutParams().height = height;
+        photo.getLayoutParams().width = width;
+
+
+        photo.requestLayout();
+    }
+
+}
