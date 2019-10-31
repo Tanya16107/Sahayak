1b0fe403e3b6986c59cc5209ddd51d37a9e15dfb
Viresh Gupta <viresh16118@iiitd.ac.in>
2019-11-01 01:19:26 +0530

    Not to be merged
    Only for sharing docker information

---
+++ openvidu_extras/script.sh
@@ -0,0 +1,5 @@
+# openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -subj '/CN=sahayak.github.io/O=MCGroup6/C=IN' -keyout key.pem -out cert.pem
+# password:password
+# openssl pkcs12 -export -name key1 -in cert.pem -inkey key.pem -out p12keystore.p12
+# keytool -importkeystore -srckeystore p12keystore.p12 -srcstoretype pkcs12 -deststoretype pkcs12 -alias key1 -destkeystore openvidu.jks
+docker-compose -d up
