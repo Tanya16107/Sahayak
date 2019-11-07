2d3f1a3c8780dbb74ab5aefb6b71906d311b24a1
Jigme Lobsang lepcha <jigme16045@iiitd.ac.in>
2019-11-07 16:28:50 +0530

    Adds all active sessions to the EditProposalActivity

--- app/src/main/java/com/mobilecomputing/sahayak/Activities/EditProposalActivity.java
+++ app/src/main/java/com/mobilecomputing/sahayak/Activities/EditProposalActivity.java
@@ -4,10 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
-import com.mobilecomputing.sahayak.R;
-public class EditProposalActivity extends AppCompatActivity {
+import android.util.Log;
+import android.view.View;
+import android.widget.SearchView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.mobilecomputing.sahayak.Adapters.ProposalAdapter;
+import com.mobilecomputing.sahayak.JavaClasses.Proposal;
+import com.mobilecomputing.sahayak.JavaClasses.ProposalLab;
+import com.mobilecomputing.sahayak.R;
+
+import java.util.List;
+
+public class EditProposalActivity extends AppCompatActivity {
+    public static List<Proposal> proposals;
+    ProposalAdapter mAdapter;
+    SearchView searchView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,7 +30,20 @@ public class EditProposalActivity extends AppCompatActivity {
 
         RecyclerView mentorProposalView = (RecyclerView) findViewById(R.id.edit_proposal);
         mentorProposalView.setLayoutManager(new LinearLayoutManager(this));
+        ProposalLab proposalLab = ProposalLab.get(getApplicationContext());
+        proposals = proposalLab.getProposals();
 
-
+        mAdapter = new ProposalAdapter(this, proposals);
+        mAdapter.setClickListener(new ProposalAdapter.ItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Proposal p = mAdapter.getItem(position);
+                Intent intent = new Intent(EditProposalActivity.this, IndividualProposalActivity.class);
+                startActivity(intent);
+            }
+        });
+        mentorProposalView.setAdapter(mAdapter);
     }
+
+
 }
